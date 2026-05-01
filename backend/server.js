94de0e6b8363015a42a553d33e25afd9a190499b
Omar Bahgat <omar_bahgat@aucegypt.edu>
2026-05-01 16:53:13 +0300

    Add frontend polling logic for sensor updates

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,29 @@
 const express = require('express');
+const cors = require('cors');
 
 const app = express();
+app.use(cors());
 app.use(express.json());
+
+let state = {
+    occupied: false,
+};
 
 app.post('/sensor', (req, res) => {
     const { occupied } = req.body;
+
+    state.occupied = occupied;
 
     console.log('Occupied:', occupied);
 
     res.sendStatus(200);
 });
 
+app.get('/sensor', (req, res) => {
+    res.json(state);
+});
+
+// Start server
 const PORT = 3000;
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
